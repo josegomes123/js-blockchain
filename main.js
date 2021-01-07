0382139046c3d9561de62a9260aa30a1a9caf07d
José Gomes <zegomes@gmail.com>
2021-01-07 20:36:36 +0000

    Fixed base blockchain

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
 class Block {
-	contructor(timestamp, transactions, previousHash = '') {
-		this.timestamp = timestamp;
-		this.transactions = transactions;
-		this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-	}
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
 
-	// Calculate unique hash of block from properties
-	calculateHash() {
-		return SHA256(
-			this.index +
-				this.previousHash +
-				this.timestamp +
-				JSON.stringify(this.data)
-		).toString();
-	}
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
 }
 
 class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-      }
-    
+  constructor() {
+    this.blockchain = [this.createGenesisBlock()];
+  }
 
-	createGenesisBlock() {
-		return new Block(Date.parse('07/01/2021'), [], '0');
-	}
+  createGenesisBlock() {
+    return new Block(0, "01/01/2020", "Initial Block in the Chain", "0");
+  }
 
-	getLatestBlock() {
-		return this.chain[this.chain.length -1];
-	}
+  getLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
 
-	addBlock(newBlock) {
-		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
-		this.chain.push(newBlock);
-	}
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.blockchain.push(newBlock);
+  }
+
 }
 
-const jsCoin = new Blockchain();
+let jsCoin = new Blockchain();
 
+jsCoin.addBlock(
+  new Block(1, "01/06/2020", {
+    sender: "Test1",
+    recipient: "Test2",
+    quantity: 33
+  })
+);
+
+jsCoin.addBlock(
+  new Block(2, "01/07/2020", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100
+  })
+);
 
 console.log(JSON.stringify(jsCoin, null, 4));
