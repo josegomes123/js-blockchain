557f3cb98b891aadd261a540a6c822ef939fd39e
José Gomes <zegomes@gmail.com>
2021-01-13 00:53:49 +0000

    Added Proof Of Work

--- main.js
+++ main.js
@@ -19,11 +19,27 @@ class Block {
 				this.nonce
 		).toString();
 	}
+
+  // Proof Of Work algorithm
+	mineBlock(difficulty) {
+    // Create array of 0s with difficulty
+		let array0s = new Array(difficulty);
+    for (let i = 0; i < difficulty; ++i) array0s[i] = 0;
+    // Block is mined when calculateHash() substring of length "difficulty" matches all 0s
+    // Hash is changed with nonce
+    // Higher Difficulty increases solving time exponentially
+		while (this.hash.substring(0, difficulty) !== array0s.join("")) {
+			this.nonce++;
+			this.hash = this.calculateHash();
+		}
+		console.log('Block mined: ' + this.hash);
+	}
 }
 
 class Blockchain {
 	constructor() {
-		this.blockchain = [this.createGenesisBlock()];
+    this.blockchain = [this.createGenesisBlock()];
+    this.difficulty = 4;
 	}
 
 	createGenesisBlock() {
@@ -35,21 +51,21 @@ class Blockchain {
 	}
 
 	addBlock(newBlock) {
-		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
 		this.blockchain.push(newBlock);
 	}
 
-  // Test integrity of blockchain
+	// Test integrity of blockchain
 	isChainValid() {
 		for (let i = 1; i < this.blockchain.length; i++) {
 			const currentBlock = this.blockchain[i];
-      const previousBlock = this.blockchain[i - 1];
-      // Hash of each block has to be valid
+			const previousBlock = this.blockchain[i - 1];
+			// Hash of each block has to be valid
 			if (currentBlock.hash !== currentBlock.calculateHash()) {
 				return false;
-      }
-      // Hashes of chained blocks must match
+			}
+			// Hashes of chained blocks must match
 			if (currentBlock.previousHash !== previousBlock.hash) {
 				return false;
 			}
@@ -60,6 +76,7 @@ class Blockchain {
 
 let jsCoin = new Blockchain();
 
+console.log("Mining block 1:");
 jsCoin.addBlock(
 	new Block(1, '01/06/2020', {
 		sender: 'Test1',
@@ -68,6 +85,7 @@ jsCoin.addBlock(
 	})
 );
 
+console.log("Mining block 2:");
 jsCoin.addBlock(
 	new Block(2, '01/07/2020', {
 		sender: 'Vitaly Friedman',
