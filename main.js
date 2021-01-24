71ac819b3dfd42aece6034c2aaf432fc1eebdd33
José Gomes <zegomes@gmail.com>
2021-01-24 23:47:41 +0000

    Added mining rewards and transactions

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+	constructor(fromAddress, toAddress, amount) {
+		this.fromAddress = fromAddress;
+		this.toAddress = toAddress;
+		this.amount = amount;
+	}
+}
+
 class Block {
-	constructor(index, timestamp, data, previousHash = '') {
-		this.index = index;
+	constructor(timestamp, transactions, previousHash = '') {
 		this.timestamp = timestamp;
-		this.data = data;
+		this.transactions = transactions;
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
 		this.nonce = 0;
@@ -15,20 +22,20 @@ class Block {
 			this.index +
 				this.previousHash +
 				this.timestamp +
-				JSON.stringify(this.data) +
+				JSON.stringify(this.transactions) +
 				this.nonce
 		).toString();
 	}
 
-  // Proof Of Work algorithm
+	// Proof Of Work algorithm
 	mineBlock(difficulty) {
-    // Create array of 0s with difficulty
+		// Create array of 0s with difficulty
 		let array0s = new Array(difficulty);
-    for (let i = 0; i < difficulty; ++i) array0s[i] = 0;
-    // Block is mined when calculateHash() substring of length "difficulty" matches all 0s
-    // Hash is changed with nonce
-    // Higher Difficulty increases solving time exponentially
-		while (this.hash.substring(0, difficulty) !== array0s.join("")) {
+		for (let i = 0; i < difficulty; ++i) array0s[i] = 0;
+		// Block is mined when calculateHash() substring of length "difficulty" matches all 0s
+		// Hash is changed with nonce
+		// Higher Difficulty increases solving time exponentially
+		while (this.hash.substring(0, difficulty) !== array0s.join('')) {
 			this.nonce++;
 			this.hash = this.calculateHash();
 		}
@@ -38,22 +45,47 @@ class Block {
 
 class Blockchain {
 	constructor() {
-    this.blockchain = [this.createGenesisBlock()];
-    this.difficulty = 4;
+		this.blockchain = [this.createGenesisBlock()];
+		this.difficulty = 2;
+		this.pendingTransaction = [];
+		this.miningReward = 100;
 	}
 
 	createGenesisBlock() {
-		return new Block(0, '01/01/2020', 'Initial Block in the Chain', '0');
+		return new Block('01/01/2020', 'Initial Block in the Chain', '0');
 	}
 
 	getLatestBlock() {
 		return this.blockchain[this.blockchain.length - 1];
 	}
 
-	addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.mineBlock(this.difficulty);
-		this.blockchain.push(newBlock);
+	minePendingTransaction(miningRewardAddress) {
+		let block = new Block(Date.now(), this.pendingTransaction);
+		block.mineBlock(this.difficulty);
+		console.log('Block successfully mined!');
+		this.blockchain.push(block);
+		this.pendingTransaction = [
+			new Transaction(null, miningRewardAddress, this.miningReward),
+		];
+	}
+
+	createTransaction(transaction) {
+		this.pendingTransaction.push(transaction);
+	}
+
+	// check the entire blockchain for your address balance
+	getBalanceOfAddress(address) {
+		let balance = 0;
+		for (const block of this.blockchain) {
+			for (const transaction of block.transactions) {
+				if (transaction.fromAddress === address) {
+					balance -= transaction.amount;
+				} else if (transaction.toAddress === address) {
+					balance += transaction.amount;
+				}
+			}
+		}
+		return balance;
 	}
 
 	// Test integrity of blockchain
@@ -75,29 +107,16 @@ class Blockchain {
 }
 
 let jsCoin = new Blockchain();
-
-console.log("Mining block 1:");
-jsCoin.addBlock(
-	new Block(1, '01/06/2020', {
-		sender: 'Test1',
-		recipient: 'Test2',
-		quantity: 33,
-	})
+jsCoin.createTransaction(new Transaction('address1', 'address2', 100));
+jsCoin.createTransaction(new Transaction('address2', 'address1', 100));
+jsCoin.createTransaction(new Transaction('address2', 'address3', 100));
+console.log('Starting miner');
+jsCoin.minePendingTransaction('miner-address');
+console.log(
+	'Balance of miner is ' + jsCoin.getBalanceOfAddress('miner-address')
 );
-
-console.log("Mining block 2:");
-jsCoin.addBlock(
-	new Block(2, '01/07/2020', {
-		sender: 'Vitaly Friedman',
-		recipient: 'Ricardo Gimenes',
-		quantity: 100,
-	})
+console.log('Starting miner again');
+jsCoin.minePendingTransaction('miner-address');
+console.log(
+	'Balance of miner is ' + jsCoin.getBalanceOfAddress('miner-address')
 );
-
-// console.log(JSON.stringify(jsCoin, null, 4));
-
-// console.log("is chain valid " + jsCoin.isChainValid());
-
-// jsCoin.blockchain[1].data = {amount:100};
-// console.log("is chain valid " + jsCoin.isChainValid());
-// jsCoin.blockchain[1].hash = savje
